d561ecc647e9ab2e6876a5fd6c2c19459e7a3270
SOFTTEKCLIENTS\marcop.perez <marcop.perez@softtek.com>
2018-07-13 15:36:41 -0500

    Se agregan datos fake a las tablas de resultados y se muestran solamente los que son declarados como display_result=true

--- casperjstestsample.js
+++ casperjstestsample.js
@@ -2,55 +2,130 @@ var screensDir = 'testing/screenshoots/CasperTest/ClienteTest/create/'
 var screensCaptureDir = 'testing/captures/CasperTest/ClienteTest/create/'
 var stepCounter = 0
 var stepCaptureCounter = 0
+var spannames=[]
+var spanname='' 
 
-casper.test.begin('Creates a Model for ClienteTest (Principal Scenario)', 5, function (test) {
-  // trigger: Administrator chooses Add ClienteTest Page
+casper.test.begin('Creates a Model for ClienteTest (Principal Scenario)', 11, function (test) {
+  
+  // Trigger. El Operador solicita la página [Crear ClienteTest].
   casper.start('http://localhost:1337/#!/clientetest-add/', function () {
+  	this.echo("Trigger. El Operador solicita la página [Crear ClienteTest].")
     this.viewport(1280, 850)
   })
 
-  // Step 01. System shows ClienteTest and Empty Fields
+
+  // Step 01. El Sistema muestra la página [Crear ClienteTest].
   casper.then(function () {
+  	this.echo("Step 01. El Sistema muestra la página [Crear ClienteTest].")
     test.assertSelectorHasText('#clientetest-add', 'Agregar ClienteTest')
   })
   
-  
   casper.then(function () {
-      test.assertSelectorHasText('input#numero-clientetest', '')
+      test.assertSelectorHasText('input#Direccion', '')
   })
   casper.then(function () {
-      test.assertSelectorHasText('input#nombre-clientetest', '')
+      test.assertSelectorHasText('input#Domicilio', '')
   })
   casper.then(function () {
-      test.assertSelectorHasText('input#direccion-clientetest', '')
+      test.assertSelectorHasText('input#Calle', '')
+  })
+  casper.then(function () {
+      test.assertSelectorHasText('input#CodigoPostal', '')
   })
 
-  // Step02. Administrator enters ClienteTest
-  casper.then(function () {
+
+ // Step02. El Operador captura información en la forma [Crear ClienteTest].
+ casper.then(function () {
+ 	this.echo("Step02. El Operador captura información en la forma [Crear ClienteTest].")
     this.fill('formbox form', {
-      'Numero' : 'ClienteTest X Value' ,
-      'Nombre' : 'ClienteTest X Value' ,
       'Direccion' : 'ClienteTest X Value' ,
+      'Domicilio' : 'ClienteTest X Value' ,
+      'Calle' : 'ClienteTest X Value' ,
+      'CodigoPostal' : 'ClienteTest X Value' ,
       
     }, false)
   })
-
-  // Step03. Administrator chooses ClienteTest 
+ 
+ casper.then(function () {
+ test.assertSelectorHasText('input#Direccion', 'ClienteTest X Value')
+ })
+ 
+ casper.then(function () {
+ test.assertSelectorHasText('input#Domicilio', 'ClienteTest X Value')
+ })
+ 
+ casper.then(function () {
+ test.assertSelectorHasText('input#Calle', 'ClienteTest X Value')
+ })
+ 
+ casper.then(function () {
+ test.assertSelectorHasText('input#CodigoPostal', 'ClienteTest X Value')
+ })
+  
+  
+  // Step03. El Sistema determina que los datos de la forma [Crear ClienteTest] están completos.
   casper.then(function () {
+  	this.echo("Step03. El Sistema determina que los datos de la forma [Crear ClienteTest] están completos.")
+      spanname = this.evaluate(function() {
+      		       var names = $('span#Direccion2').text()
+      		       return names
+      		       });
+      		  
+      spannames.push(spanname);
+      
+      spanname = this.evaluate(function() {
+      		       var names = $('span#Domicilio2').text()
+      		       return names
+      		       });
+      		  
+      spannames.push(spanname);
+      
+      spanname = this.evaluate(function() {
+      		       var names = $('span#Calle2').text()
+      		       return names
+      		       });
+      		  
+      spannames.push(spanname);
+      
+      spanname = this.evaluate(function() {
+      		       var names = $('span#CodigoPostal2').text()
+      		       return names
+      		       });
+      		  
+      spannames.push(spanname);
+      
+       //this.echo(spannames.join().replace(/,/g, ''));
+       //this.echo(spannames.join().replace(/,/g, '').length);
+       //test.assert(spannames.join().replace(/,/g, '')=='')
+       test.assert(spannames.join().indexOf('Campo requerido') === -1)
+   })
+    
+  
+  // Step04. El Sistema determina que los datos de la forma [Crear ClienteTest] son válidos.
+  casper.then(function () { 
+     this.echo("Step04. El Sistema determina que los datos de la forma [Crear ClienteTest] son válidos.")
+     //this.echo(spannames.join().replace(/,/g, '')!='')
+     //this.echo(spannames.join().indexOf('Campo requerido') !== -1) 
+     test.assert(!(spannames.join().replace(/,/g, '')!='' && spannames.join().indexOf('Campo requerido') === -1))  
+   })
+    
+  
+  // Step05. El Operador elige el comando [Guardar].
+  casper.then(function () {
+  	this.echo("Step05. El Operador elige el comando [Guardar].")
     casper.click('a.btn')
   })
 
-  // Step04. System validates ClienteTest 
 
-  // Step05. System creates ClienteTest 
-
-  // Post-condition
+  // Step06. El Sistema crea un nuevo registro en la entidad [ClienteTest].
   casper.then(function () {
+  	this.echo("Step06. El Sistema crea un nuevo registro en la entidad [ClienteTest].")
     test.assertSelectorHasText('#clientetest-admin', 'Administrar ClienteTest')
   })
   
-  // End of Use Case
+  // Step07. Fin del Caso de Uso.
   casper.run(function () {
+  	this.echo("Step07. Fin del Caso de Uso.")
     test.done()
   })
 })
--- src/components/app/clientetest/clientetest-add.tag
+++ src/components/app/clientetest/clientetest-add.tag
@@ -1,24 +1,31 @@
 
 <clientetest-add>
 	<page id="clientetest-add" title="Agregar ClienteTests">
-		<formbox  title="Datos del ClienteTest" icon="fa fa-check-circle-o" action="create" return="clienteTest-admin">
+		<formbox title="Datos del ClienteTest" icon="fa fa-check-circle-o" action="create" return="clienteTest-admin">
 			<div class="row">
-				<div class="col-md-4">
-				<inputbox id="Numero" type="number" pattern="[0-9]" onkeypress="return event.charCode >= 48 && event.charCode <= 57" label="Numero" value="" placeholder="Numero del ClienteTest" required=true disabled=false minsize=3 maxsize=100 />
+				<div class="col-md-4">	
+				<inputbox id="Direccion" type="text" label="Direccion" value="" placeholder="Direccion del ClienteTest" required=true disabled=false />
 				</div>
 			
 			<p hidden>1</p>
 				<div class="col-md-4">	
-				<inputbox id="Nombre" type="text" label="Nombre" value="" placeholder="Nombre del ClienteTest" required=true disabled=false />
+				<inputbox id="Domicilio" type="text" label="Domicilio" value="" placeholder="Domicilio del ClienteTest" required=true disabled=false />
 				</div>
 			
 			<p hidden>2</p>
 				<div class="col-md-4">	
-				<inputbox id="Direccion" type="text" label="Direccion" value="" placeholder="Direccion del ClienteTest" required=true disabled=false />
+				<inputbox id="Calle" type="text" label="Calle" value="" placeholder="Calle del ClienteTest" required=true disabled=false />
 				</div>
 			</div>
 			
 			<p hidden>3</p>
+			<div class="row">
+				<div class="col-md-4">	
+				<inputbox id="CodigoPostal" type="text" label="CodigoPostal" value="" placeholder="CodigoPostal del ClienteTest" required=true disabled=false />
+				</div>
+			</div>
+			
+			<p hidden>4</p>
 		</formbox>
 	</page>
 </clientetest-add>	
